fc49c8c4b17bfb756747eee3bbad8b5565d33127
Damon Choi <34573040+NazoxD@users.noreply.github.com>
2018-10-10 15:43:28 +1300

    Update home.js

--- home.js
+++ home.js
@@ -16,3 +16,12 @@ $(document).ready(function(){
     } 
   });
 });
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("colourfill").style.width = scrolled + "%";
+}
